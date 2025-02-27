734fc37b4aa36aeebab23faa1f9611a473dc95b6
Anuradha Bandara <bandara00007@gmail.com>
2025-02-27 12:19:56 +0800

    refactor: change remotes url

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
       name: "hostApp",
       filename: "remoteEntry.js",
       remotes: {
-        remoteApp: "/remote-users/assets/remoteEntry.js",
-        remoteCharts: "/remote-charts/assets/remoteEntry.js",
+        remoteApp: "https://kumudunibandara.github.io/remote-users/assets/remoteEntry.js",
+        remoteCharts: "https://kumudunibandara.github.io/remote-charts/assets/remoteEntry.js",
       },
       exposes: {
         "./UserTheme": "./src/redux/reducer",
